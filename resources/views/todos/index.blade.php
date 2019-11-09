6ccecaebdf30ab31762aa3d02a6147dd94b0fc29
cedrickevers <keverscedric@gmail.com>
2019-11-09 12:48:47 +0100

    refactoring index and show with @yield

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ @extends ("layouts.app")
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+ @section("content")
 
-
-    <title>Todos</title>
-</head>
-<body>
-<div class="container">
-    <div class="row justify-content-center">
+ <div class="row justify-content-center">
 <div class="card col-md-5">
 
 <h1 class="text-center">Todos Page </h1>
@@ -27,7 +17,4 @@
 </div>
 
     </div>
-</div>
-
-</body>
-</html>
+    @endsection
--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+ @extends("layouts.app")
 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
 
-<div class="container bg-secondary text-white">
-<div class="row">
+ @section("content")
+ <div class="row">
     <div class="col-md-6 justify-content-center">
 
     <h2 class="text-center my-5">
@@ -29,6 +20,5 @@
     </div>
     </div>
 </div>
-</div>
-</body>
-</html>
+ @endsection()
+
